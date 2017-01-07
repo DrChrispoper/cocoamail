d0815022dae944cdda4b5b8c664ca73469aedb97
Andy Cerier <gtdandy@cognitivepuppy.com>
2017-01-07 10:54:14 -0500

    Changed IndexSet from a struct to a class
    For safer property handling and easier debugging.

--- CocoaMail/Backend/DataBaseCore/Models/CCMIndexSet.h
+++ CocoaMail/Backend/DataBaseCore/Models/CCMIndexSet.h
@@ -5,9 +5,22 @@
 //  Created by Andrew Cerier on 1/4/17.
 //  Copyright © 2017 Christopher Hockley. All rights reserved.
 //
-
+// CCMIndexSetArray contains a mutable Array of mutable IndexSets, one per account
+//      where each IndexSet is a mutable set of mail/conversation indecies for that account
+//
 #import <Foundation/Foundation.h>
 
-@interface CCMIndexSet : NSObject
+@class ConversationIndex;
+
+@interface CCMIndexSetArray : NSObject
+
+- (instancetype)initWithCapacity:(NSInteger)capacity;
+-(void) appendIndexSetToArray;  // Append an account mail index set for each non-All account
+-(NSInteger)indexSetCount;
+-(NSInteger)totalIndexCount;
+-(void)addConversationIndex:(ConversationIndex*)ci;
+-(void)removeConversationIndex:(ConversationIndex*)ci;
+-(BOOL)containsConversationIndex:(ConversationIndex*)ci;
+-(NSString*)description;
 
 @end
